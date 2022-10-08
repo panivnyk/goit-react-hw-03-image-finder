2e739ff1302190bcf6f1a5f3f4929668ccc24cc9
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-10-09 00:03:24 +0300

    some add for start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { fetchImages } from 'components/services/api.jsx';
+// import {} from './App.styled';
+
+export class App extends Component {
+  state = {
+    searchQuery: '',
+    currentPage: 1,
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.page !== this.state.page ||
+      prevState.query !== this.state.query
+    ) {
+      this.loadImages(this.state.query, this.state.page);
+    }
+  }
+
+  componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?key=29451964-958278d8f10d2abadadf36c5e&q=cat&image_type=photo&orientation=horizontal&per_page=12&page=1'
+    )
+      .then(res => res.json())
+      .then(console.log);
+  }
+
+  render() {
+    return <div>test</div>;
+  }
+}
