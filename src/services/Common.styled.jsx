4a48976c2de49083c2b7799e116fa7806e65a399
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-10-11 00:23:07 +0300

    HW for check

--- src/services/Common.styled.jsx
+++ src/services/Common.styled.jsx
@@ -175,7 +175,9 @@ export const ButtonLoadMore = styled.button`
   line-height: 24px;
   font-style: normal;
   font-weight: 500;
-  min-width: 180px;
+  width: 180px;
+  margin-left: auto;
+  margin-right: auto;
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
 
